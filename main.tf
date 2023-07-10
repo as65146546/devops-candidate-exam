14d5313eb958725bdea39648949115071f855d0f
flaccx <as65146546@gmail.com>
2023-07-10 09:16:05 +0300

    tf plan

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@ resource "aws_route" "private_route" {
   nat_gateway_id         = data.aws_nat_gateway.nat.id
 }
 
-resource "aws_lambda_function" "my_lambda" {
-  function_name = "my-lambda-function"
-  role          = data.aws_iam_role.lambda.arn
-  filename      = "lambda_function.zip"  
-}
+# resource "aws_lambda_function" "my_lambda" {
+#   function_name = "my-lambda-function"
+#   role          = data.aws_iam_role.lambda.arn
+#   filename      = "lambda_function.zip"  
+# }
 
-resource "aws_security_group" "my_security_group" {
-  name        = "my-security-group"
-  description = "My security group"
-  vpc_id      = data.aws_vpc.vpc.id
+# resource "aws_security_group" "my_security_group" {
+#   name        = "my-security-group"
+#   description = "My security group"
+#   vpc_id      = data.aws_vpc.vpc.id
   
 }
 
